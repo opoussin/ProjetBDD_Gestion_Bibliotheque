f349beff4b6847f36da469db37cbd9ecd9e85b49
Lauriane Rey <lauriane.rey@etu.utc.fr>
2022-11-10 10:48:26 +0000

    Update Rendu3/SqlCreateV1.sql

--- Rendu3/SqlCreateV1.sql
+++ Rendu3/SqlCreateV1.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Compte_utilisateur (
   PRIMARY KEY login,
   Mot_de_passe VARCHAR
-)
+);
 
 
 
@@ -21,7 +21,7 @@ CREATE TABLE Adherents(
   Nb_retard INTEGER,
   Nb_degradation INTEGER,
   carte VARCHAR
-)
+);
 
 CREATE TABLE Personnel(
   FOREIGN KEY login REFERENCES Compte_utilisateur(login),
@@ -29,13 +29,13 @@ CREATE TABLE Personnel(
   Prenom VARCHAR NOT NULL,
   Adresse VARCHAR NOT NULL,
   Mail VARCHAR UNIQUE
-)
+);
 
 CREATE TABLE Adhesions(
   FOREIGN KEY login REFERENCES Adherents(login),
   Debut DATE NOT NULL,
   FIN DATE
-)
+);
 
 CREATE TABLE Ressource(
   PRIMARY KEY Code INTEGER,
@@ -54,27 +54,26 @@ CREATE TABLE Ressource(
   Livre BOOLEAN,
   Film BOOLEAN,
   Oeuvremusicale BOOLEAN
-  CHECK ((Livre NOT NULL AND ISBN NOT NULL AND Langue_livre NOT NULL AND Résumé NOT NULL)
-        OR (Oeuvremusicale NOT NULL AND Durée_oeuvre NOT NULL)
-        OR (Film NOT NULL AND Synopsis NOT NULL AND Langue_film NOT NULL AND Durée_film NOT NULL)),
-)
+  CHECK ((Livre NOT NULL AND ISBN NOT NULL AND Langue_livre NOT NULL AND Résumé NOT NULL AND Oeuvremusicale IS NULL AND Durée_oeuvre IS NULL AND Film IS NULL AND Synopsis IS NULL AND Langue_film IS NULL AND Durée_film IS NULL)
+        OR (Oeuvremusicale NOT NULL AND Durée_oeuvre NOT NULL AND Livre IS NULL AND ISBN IS NULL AND Langue_livre IS NULL AND Résumé IS NULL AND Film IS NULL AND Synopsis IS NULL AND Langue_film IS NULL AND Durée_film IS NULL )
+        OR (Film NOT NULL AND Synopsis NOT NULL AND Langue_film NOT NULL AND Durée_film NOT NULL AND Livre IS NULL AND ISBN IS NULL AND Langue_livre IS NULL AND Résumé IS NULL AND Oeuvremusicale IS NULL AND Durée_oeuvre IS NULL )),
+);
 
 CREATE TABLE Contributeur (
-  Rôle VARCHAR,
   Nom VARCHAR,
   Prenom VARCHAR,
   Date_de_naissance DATE,
   PRIMARY KEY (Rôle, Nom, Prenom, Date_de_naissance),
   Nationalité VARCHAR
-)
+);
 
 CREATE TABLE Contribue (
-  FOREIGN KEY Rôle REFERENCES Contributeur(Rôle),
+  Rôle role,
   FOREIGN KEY Code REFERENCES Ressource(code),
   FOREIGN KEY Nom REFERENCES Contributeur(Nom),
   FOREIGN KEY Prenom REFERENCES Contributeur(Prenom),
   FOREIGN KEY Date_de_naissance REFERENCES Contributeur(Date_de_naissance),
-)
+);
 
 CREATE TABLE Exemplaire(
   PRIMARY KEY Clé VARCHAR,
@@ -82,7 +81,7 @@ CREATE TABLE Exemplaire(
   État etat NOT NULL,
   Disponibilité BOOLEAN ,
   compteur INTEGER NOT NULL
-)
+);
 
 CREATE TABLE EMPRUNT (
   FOREIGN KEY Clé REFERENCES Exemplaire(Clé),
@@ -90,14 +89,14 @@ CREATE TABLE EMPRUNT (
   emprunt_enCours BOOLEAN,
    Durée_limite DATE,
   Etat_retour etat
-)
+);
 
 CREATE TABLE Reservation (
   FOREIGN KEY Clé REFERENCES Exemplaire(Clé),
   FOREIGN KEY login REFERENCES Adherents(login),
   état_reservation BOOLEAN,
   reserv_date DATE
-)
+);
 
 CREATE TABLE Sanction(
   FOREIGN KEY login REFERENCES Adhérents(login),
@@ -110,5 +109,5 @@ CREATE TABLE Sanction(
   CHECK (En_Retard NOT NULL AND Debut_retard NOT NULL),
   CHECK(En_Degradation NOT NULL AND Debut_degradation NOT NULL),
   CHECK((En_sanction NOT NULL AND En_Retard NOT NULL) OR (En_sanction NOT NULL AND En_Degradation NOT NULL))
-)
+);
 
