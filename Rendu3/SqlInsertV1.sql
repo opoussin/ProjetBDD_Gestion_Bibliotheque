620f74c1a2ebcaccfb1fd89ad84c816b0e6f8b75
Lauriane Rey <lauriane.rey@etu.utc.fr>
2022-11-12 17:13:30 +0000

    Update Rendu3/SqlInsertV1.sql, Rendu3/sql_ressourcesetexemplaires_insert.sql

--- Rendu3/SqlInsertV1.sql
+++ Rendu3/SqlInsertV1.sql
@@ -1,1 +1,12 @@
+INSERT INTO Ressource VALUES ('Les Fleurs du Mal', 'Pierre Brunel', 'Poésie', '1868','3', '9782702182888', 'Français', 'recueil de poèmes de Charles Baudelaire, reprenant la quasi-totalité de sa production en vers de 1840 jusque sa mort, survenue fin août 1867
+','NULL','NULL','NULL','NULL', 'Livre');
+
+INSERT INTO Ressource VALUES ('Les Fleurs du Mal', 'Pierre Brunel', 'Poésie', '1868','3', '9782702182888', 'Français', 'recueil de poèmes de Charles Baudelaire, reprenant la quasi-totalité de sa production en vers de 1840 jusque sa mort, survenue fin août 1867
+','NULL','NULL','NULL','NULL', 'Livre');
+
+INSERT INTO Ressource VALUES ('Les Fleurs du Mal', 'Pierre Brunel', 'Poésie', '1868','3', '9782702182888', 'Français', 'recueil de poèmes de Charles Baudelaire, reprenant la quasi-totalité de sa production en vers de 1840 jusque sa mort, survenue fin août 1867
+','NULL','NULL','NULL','NULL', 'Livre');
+
+INSERT INTO Ressource VALUES ('Les Fleurs du Mal', 'Pierre Brunel', 'Poésie', '1868','3', '9782702182888', 'Français', 'recueil de poèmes de Charles Baudelaire, reprenant la quasi-totalité de sa production en vers de 1840 jusque sa mort, survenue fin août 1867
+','NULL','NULL','NULL','NULL', 'Livre');
 
