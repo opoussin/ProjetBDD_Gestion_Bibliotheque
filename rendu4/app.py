da1b9f7addfcb817f56d475820025f66593c995b
Lauriane Rey <lauriane.rey@etu.utc.fr>
2022-11-17 10:55:40 +0000

    Update rendu4/app.py

--- rendu4/app.py
+++ rendu4/app.py
@@ -6,11 +6,40 @@ conn = psycopg2.connect("dbname='mydb' user='me' host='localhost' password='secr
 cur = conn.cursor()
 
 login_user = input("Entrez votre login : ")
-
-if login_user
-
-sql = "SELECT login FROM Adherents" % (num, dpt, pop)
+sql = "SELECT login FROM Adherents, Personnel WHERE login=%s" %login_user
 cur.execute(sql)
+
+//login faux : 
+while(sql='NULL')
+    print( "le login n'est pas bon")
+    login_user = input("Entrez votre login : ")
+    sql = "SELECT login FROM Adherents, Personnel WHERE login=%s" %login_user
+    cur.execute(sql)
+
+
+
+sql2="SELECT login FROM Adherents WHERE login=%s" %login_user
+cur.execute(sql2)
+    if sql2='NULL'
+        sql2 = "SELECT login FROM Personnel WHERE login=%s" %login_user
+        cur.execute(sql2)
+        /*MENU Personnel
+    else 
+        /* menu adhérents
+
+
+
+sql = "SELECT login FROM Adherents WHERE login=%s" %login_user
+cur.execute(sql)
+    if sql='NULL'
+        sql2 = "SELECT login FROM Personnel WHERE login=%s" %login_user
+        if sql2=NULL
+            login_user = input("Le login donné n'existe pas dans la base de données, entrez votre login : ")
+
+
+
+
+
 conn.commit()
 
 conn.close()
