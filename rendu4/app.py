6c0db4b20becedd5e76ea29dadf3420b5bfe10f1
Lauriane Rey <lauriane.rey@etu.utc.fr>
2022-11-17 11:03:05 +0000

    Update rendu4/app.py

--- rendu4/app.py
+++ rendu4/app.py
@@ -8,33 +8,26 @@ cur = conn.cursor()
 login_user = input("Entrez votre login : ")
 sql = "SELECT login FROM Adherents, Personnel WHERE login=%s" %login_user
 cur.execute(sql)
+row = cur.fetchone()
 
-//login faux : 
-while(sql='NULL')
+#login faux : 
+while(!row)
     print( "le login n'est pas bon")
     login_user = input("Entrez votre login : ")
     sql = "SELECT login FROM Adherents, Personnel WHERE login=%s" %login_user
     cur.execute(sql)
+    row = cur.fetchone()
 
 
-
+#on cherche si c''est un adhérent ou un personnel
 sql2="SELECT login FROM Adherents WHERE login=%s" %login_user
 cur.execute(sql2)
-    if sql2='NULL'
-        sql2 = "SELECT login FROM Personnel WHERE login=%s" %login_user
-        cur.execute(sql2)
+row = cur.fetchone()
+    if (!row)
         /*MENU Personnel
     else 
         /* menu adhérents
 
-
-
-sql = "SELECT login FROM Adherents WHERE login=%s" %login_user
-cur.execute(sql)
-    if sql='NULL'
-        sql2 = "SELECT login FROM Personnel WHERE login=%s" %login_user
-        if sql2=NULL
-            login_user = input("Le login donné n'existe pas dans la base de données, entrez votre login : ")
 
 
 
