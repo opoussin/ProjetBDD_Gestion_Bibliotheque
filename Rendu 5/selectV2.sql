e68ceda389ae40ebf40bf38e5990080ebe54d4ff
Lauriane Rey <lauriane.rey@etu.utc.fr>
2022-12-19 11:30:37 +0000

    Update Rendu 5/selectV2.sql

--- Rendu 5/selectV2.sql
+++ Rendu 5/selectV2.sql
@@ -95,25 +95,6 @@ FROM Genre3
 
 /* Stats par auteurs ? */
 
-CREATE VIEW Auteur1 AS 
-SELECT Code, login
-FROM Emprunt JOIN Exemplaire ON Emprunt.Clé = Exemplaire.Clé
-
-
-CREATE VIEW Genre2 AS 
-SELECT Genre, Clé, login
-FROM Genre1 Join Exemplaire ON Genre1.Code = Exemplaire.Code
-
-/* On compte le nombre d'exemplaire emprunté avec le même genre*/
-CREATE VIEW Genre3 AS 
-SELECT login, Genre, COUNT(Clé) 
-FROM Genre2
-GROUP BY Genre 
-
-/*On cherche le Max de la tables ensuite dans le python*/
-CREATE VIEW Genre4 AS
-SELECT login, MAX(COUNT(Clé))
-FROM Genre3
 
 
 
