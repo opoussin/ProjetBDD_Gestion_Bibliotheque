b1884e138090bdf234a73ac7dcc744b6497de6d9
Lauriane Rey <lauriane.rey@etu.utc.fr>
2023-01-05 09:39:14 +0000

    Update rendu6/json_insert.sql, rendu6/json_create.sql, rendu6/json_select.sql

---
+++ rendu6/json_insert.sql
@@ -0,0 +1,104 @@
+INSERT INTO Compte_utilisateur VALUES ('apommep', 'arnaudpp');
+INSERT INTO Compte_utilisateur VALUES ('pduranta', 'durantpa');
+INSERT INTO Compte_utilisateur VALUES ('agatheo', 'orangecompi');
+INSERT INTO Compte_utilisateur VALUES ('denised', 'dumasdenise');
+INSERT INTO Compte_utilisateur VALUES ('arthurdr', 'papableu');
+INSERT INTO Compte_utilisateur VALUES ('juleskin', 'gateau05');
+
+INSERT INTO Adherents VALUES ('pduranta', 'durant', 'pierre', '7 Pl. de l Ancien Hôpital, 60200 Compiègne ', 'pierred@gmail.com', 0606065487 , '1988-06-06', 0,0, 'crap02');
+INSERT INTO Adherents VALUES ('agatheo', 'orange', 'agathe', '13 Rue Winston Churchill, 60200 Compiègne ', 'agatheorange@gmail.com', 0785652544 , '2000-10-26', 0,0, 'crap01');
+INSERT INTO Adherents VALUES ('arthurdr', 'drome', 'arthur', '17 Rue Winston Churchill, 60200 Compiègne ', 'dromearth@gmail.com', 0685651525 , '2001-01-17', 0,0, 'crap00');
+INSERT INTO Adherents VALUES ('juleskin', 'kimp', 'jules', '12 All. des Avenues, 60200 Compiègne', 'juleskin@orange.com', 0752652530 , '1967-04-11', 0,0, 'crap03');
+
+INSERT INTO Personnel VALUES ('apommep', 'pomme', 'arnaud', '8 Imp. de la Menardière 60200 Compiegne', 'arpomme@gmail.com');
+INSERT INTO Personnel VALUES ('denised', 'dumas', 'denise', '2-4 All. des Étangs, 60350 Vieux-Moulin', 'denisedumas@gmail.com');
+
+INSERT INTO Adhesions VALUES ('pduranta', '2020-5-4', '2022-5-16');
+INSERT INTO Adhesions VALUES ('agatheo', '2021-8-13', '2023-8-17');
+INSERT INTO Adhesions VALUES ('arthurdr', '2021-8-13', '2023-8-17');
+INSERT INTO Adhesions VALUES ('juleskin', '2022-12-15', NULL);
+
+INSERT INTO Ressource VALUES ('L1','Les Fleurs du Mal', 'Pierre Brunel', 'Poésie', 1868,3, '9782702182888', 'Français', 'recueil de poèmes de Charles Baudelaire reprenant la quasi-totalité de sa production en vers de 1840 jusque sa mort survenue fin août 1867',NULL,NULL,NULL,NULL, 'Livre');
+
+INSERT INTO Ressource VALUES ('L2','L''étranger', 'Gallimard', 'Roman', 1942,2, '9782070360024', 'Français', 'Condamné à mort, Meursault. Sur une plage algérienne, il a tué un Arabe. À cause du soleil, dira-t-il, parce qu''il faisait chaud. On n''en tirera rien d''autre. Rien ne le fera plus réagir : ni l''annonce de sa condamnation, ni la mort de sa mère, ni les paroles du prêtre avant la fin. Comme si, sur cette plage, il avait soudain eu la révélation de l''universelle équivalence du tout et du rien.
+La conscience de n''être sur la terre qu''en sursis, d''une mort qui, quoi qu''il arrive, arrivera, sans espoir de salut. Et comment être autre chose qu''indifférent à tout après ça ?',NULL,NULL,NULL,NULL, 'Livre');
+
+INSERT INTO Ressource VALUES ('L3','Les Misérables', 'Ldp jeunesse', 'Roman', 1862,4, '2010008995', 'Français', 'Le destin de Jean Valjean, forçat échappé du bagne, est bouleversé par sa rencontre avec Fantine. Mourante et sans le sou, celle-ci lui demande de prendre soin de Cosette, sa fille confiée aux Thénardier. Ce couple d’aubergistes, malhonnête et sans scrupules, exploitent la fillette jusqu’à ce que Jean Valjean tienne sa promesse et l’adopte. Cosette devient alors sa raison de vivre. Mais son passé le rattrape et l’inspecteur Javert le traque…',NULL,NULL,NULL,NULL, 'Livre');
+
+INSERT INTO Ressource VALUES ('L4','Das Parfum', 'Diogenes Verlag', 'Roman', 1985,3,'3257228007','Allemand','Die spannende Geschichte - märchenhaft, witzig und zugleich fürchterlich angsteinflössend - vom finsteren Helden Grenouille. 2006 von Tom Tykwer mit Ben Whishaw, Dustin Hoffman, Alan Rickman und Rachel Hurd-Wood in den Hauptrollen verfilmt.',NULL,NULL,NULL,NULL, 'Livre');
+
+INSERT INTO Ressource VALUES ('F1','La haine', NULL, 'Drame', 1995,1, NULL, NULL, NULL,'Abdel Ichah, seize ans est entre la vie et la mort, passé à tabac par un inspecteur de police lors d''un interrogatoire. Une émeute oppose les jeunes d''une cité HLM aux forces de l''ordre. Pour trois d''entre eux, ces heures vont marquer un tournant dans leur vie...','Français','1:38:00',NULL, 'Film');
+
+INSERT INTO Ressource VALUES ('F2','Pulp Fiction', NULL, 'Thriller', 1994,1, NULL, NULL, NULL,'L''odyssée sanglante et burlesque de petits malfrats dans la jungle de Hollywood à travers trois histoires qui s''entremêlent.','Français','2:29:00',NULL, 'Film');
+
+INSERT INTO Ressource VALUES ('F3','La Cité de la peur', NULL, 'Comédie', 1994,3, NULL, NULL, NULL,'De nos jours, à Cannes, pendant le Festival. Pas facile pour Odile Deray, petite attachée de presse de cinéma, de faire parler de son film "Red is Dead". Il faut avouer qu''il s''agit d''un film d''horreur de série Z, un petit budget aux acteurs improbables. Pourtant un jour, la chance sourit à Odile : un tueur commet des meurtres exactement de la même manière que dans son film, l''occasion est trop belle : de vrais meurtres, comme dans son film, en plein Festival de Cannes ! Comme publicité, on ne peut pas rêver mieux..','Français','1:40:00',NULL, 'Film');
+
+INSERT INTO Ressource VALUES ('F4','Le Fabuleux Destin d’’Amélie Poulain', NULL, 'Comédie', 2001,2, NULL, NULL, NULL,'Amélie, une jeune serveuse dans un bar de Montmartre, passe son temps à observer les gens et à laisser son imagination divaguer. Elle s’’est fixé un but : faire le bien de ceux qui l''entourent. Elle invente alors des stratagèmes pour intervenir incognito dans leur existence. Le chemin d''Amélie est jalonné de rencontres : Georgette, la buraliste hypocondriaque ; Lucien, le commis d''épicerie ; Madeleine Wallace, la concierge portée sur le porto et les chiens empaillés ; Raymond Dufayel alias « l''homme de verre", son voisin qui ne vit qu''à travers une reproduction d''un tableau de Renoir.','Français','2:00:00',NULL, 'Film');
+
+INSERT INTO Ressource VALUES ('OM1','Requiem', NULL, 'Classique', 1793,2, NULL, NULL, NULL,NULL,NULL,NULL,'1:00:30', 'Oeuvremusicale');
+
+INSERT INTO Ressource VALUES ('OM2','Les 4 Saisons', NULL, 'Classique', 1725,1, NULL, NULL, NULL,NULL,NULL,NULL,'0:53:00', 'Oeuvremusicale');
+
+INSERT INTO Ressource VALUES ('OM3','Marche hongroise', NULL, 'Classique', 1824,1, NULL, NULL, NULL,NULL,NULL,NULL,'0:43:00', 'Oeuvremusicale');
+
+INSERT INTO Contributeur VALUES ('Baudelaire','Charles', '1821-04-09','français');
+INSERT INTO Contributeur VALUES ('Camus','Albert', '1913-11-07','français');
+INSERT INTO Contributeur VALUES ('Hugo','Victor', '1802-02-26','français');
+INSERT INTO Contributeur VALUES ('Süskind','Patrick', '1949-03-26','Allemande');
+INSERT INTO Contributeur VALUES ('Kassovitz','Mathieu ', '1967-08-03','français');
+INSERT INTO Contributeur VALUES ('Cassel','Vincent', '1966-11-23','français');
+INSERT INTO Contributeur VALUES ('Tarantino','Quentin', '1963-03-27','Américain');
+INSERT INTO Contributeur VALUES ('Berberian','Alain', '1953-07-02','français');
+INSERT INTO Contributeur VALUES ('Chabat','Alain', '1958-11-24','français');
+INSERT INTO Contributeur VALUES ('Jeunet','Jean-Pierre', '1953-09-03','français');
+INSERT INTO Contributeur VALUES ('Mozart','Wolfgang Amadeus', '1756-01-27','autrichien');
+INSERT INTO Contributeur VALUES ('Vivaldi','Antonio Lucio', '1678-03-04','italien');
+
+INSERT INTO Contribue  VALUES ('Auteur','L1','Livre','Baudelaire','Charles', '1821-04-09');
+INSERT INTO Contribue  VALUES ('Auteur','L2','Livre','Camus','Albert', '1913-11-07');
+INSERT INTO Contribue  VALUES ('Auteur','L3','Livre','Hugo','Victor', '1802-02-26');
+INSERT INTO Contribue  VALUES ('Auteur','L4','Livre','Süskind','Patrick', '1949-03-26');
+INSERT INTO Contribue  VALUES ('Réalisateur','F1','Film','Kassovitz','Mathieu ', '1967-08-03');
+INSERT INTO Contribue  VALUES ('Acteur','F1','Film','Cassel','Vincent', '1966-11-23');
+INSERT INTO Contribue  VALUES ('Réalisateur','F2','Film','Tarantino','Quentin', '1963-03-27');
+INSERT INTO Contribue  VALUES ('Réalisateur','F3','Film','Berberian','Alain', '1953-07-02');
+INSERT INTO Contribue  VALUES ('Acteur','F3','Film','Chabat','Alain', '1958-11-24');
+INSERT INTO Contribue  VALUES ('Réalisateur','F4','Film','Jeunet','Jean-Pierre', '1953-09-03');
+INSERT INTO Contribue  VALUES ('Compositeur','OM1','Oeuvremusicale','Mozart','Wolfgang Amadeus', '1756-01-27');
+INSERT INTO Contribue  VALUES ('Compositeur','OM2','Oeuvremusicale','Vivaldi','Antonio Lucio', '1678-03-04');
+
+INSERT INTO Exemplaire VALUES ('L1_01', 'Livre', 'L1', 'Neuf',True , 0);
+INSERT INTO Exemplaire VALUES ('L1_02', 'Livre', 'L1', 'Bon',True , 0);
+INSERT INTO Exemplaire VALUES ('L1_03', 'Livre', 'L1', 'Bon',True , 0);
+
+INSERT INTO Exemplaire VALUES ('L2_01', 'Livre', 'L2', 'Bon',True , 0);
+INSERT INTO Exemplaire VALUES ('L2_02', 'Livre', 'L2', 'Bon',True , 0);
+
+INSERT INTO Exemplaire VALUES ('L3_01', 'Livre', 'L3', 'Bon',True , 0);
+INSERT INTO Exemplaire VALUES ('L3_02', 'Livre', 'L3', 'Bon',True , 0);
+INSERT INTO Exemplaire VALUES ('L3_03', 'Livre', 'L3', 'Bon',True , 0);
+INSERT INTO Exemplaire VALUES ('L3_04', 'Livre', 'L3', 'Bon',True , 0);
+
+INSERT INTO Exemplaire VALUES ('L4_01', 'Livre', 'L4', 'Bon',True , 0);
+INSERT INTO Exemplaire VALUES ('L4_02', 'Livre', 'L4', 'Bon',True , 0);
+INSERT INTO Exemplaire VALUES ('L4_03', 'Livre', 'L4', 'Bon',True , 0);
+
+INSERT INTO Exemplaire VALUES ('F1_01', 'Film', 'F1', 'Neuf',True , 0);
+
+INSERT INTO Exemplaire VALUES ('F2_01', 'Film', 'F2', 'Neuf',True , 0);
+
+INSERT INTO Exemplaire VALUES ('F3_01', 'Film', 'F3', 'Neuf',True , 0);
+INSERT INTO Exemplaire VALUES ('F3_02', 'Film', 'F3', 'Neuf',True , 0);
+INSERT INTO Exemplaire VALUES ('F3_03', 'Film', 'F3', 'Neuf',True , 0);
+
+
+INSERT INTO Exemplaire VALUES ('F4_01', 'Film', 'F4', 'Neuf',True , 0);
+INSERT INTO Exemplaire VALUES ('F4_02', 'Film', 'F4', 'Neuf',True , 0);
+
+INSERT INTO Exemplaire VALUES ('OM1_01', 'Oeuvremusicale', 'OM1', 'Neuf',True , 0);
+INSERT INTO Exemplaire VALUES ('OM1_02', 'Oeuvremusicale', 'OM1', 'Neuf',True , 0);
+
+INSERT INTO Exemplaire VALUES ('OM2_01', 'Oeuvremusicale', 'OM2', 'Neuf',True , 0);
+
+
+
---
+++ rendu6/json_select.sql
@@ -0,0 +1,126 @@
+/*Qui a contribué au film La haine ?*/
+
+SELECT Contribue.nom, Contribue.prenom, Contribue.rôle
+FROM Contribue INNER JOIN Ressource ON Contribue.code = Ressource.code
+WHERE Ressource.titre='La haine';
+
+/*Quels sont les adhérents qui ont adhéré apres le 12 novembre 2022 */
+
+SELECT Adherents.nom, Adherents.prenom
+FROM Adhesions INNER JOIN Adherents ON Adhesions.login = Adherents.login
+WHERE Adhesions.debut > '2022-11-12';
+
+/* Quels sont les personnes qui ont publié une oeuvre en même 1994 ?*/
+
+SELECT C.Nom, C.Prenom
+FROM Contribue C JOIN Ressource R ON C.Code = R.Code
+WHERE R.Date_appartion = '1994';
+
+/*VUES POUR BD*/
+
+/* View pour vérification que le login existe dans la base de donnée */
+CREATE VIEW ExistenceLogin AS
+SELECT login
+FROM Adherents  UNION SELECT login FROM Personnel ;
+
+/* View pour vérification que le login existe dans la table Adherents */
+CREATE VIEW LoginAdherent AS
+SELECT login
+FROM Adherents;
+
+/* jointure ressource _ Exemplaire */
+CREATE VIEW RessourceExemplaire AS
+SELECT Ressource.Titre, Exemplaire.Clé
+FROM Ressource INNER JOIN Exemplaire ON Ressource.Code=Exemplaire.Code ;
+
+/* Compte le nombre d'exemplaire disponible  */
+CREATE VIEW CompteExemplaire AS
+SELECT Ressource.Titre, COUNT(Exemplaire.Disponibilité)
+FROM Ressource INNER JOIN Exemplaire ON Ressource.Code=Exemplaire.Code
+WHERE Exemplaire.Disponibilité=true
+GROUP BY Ressource.Titre;
+
+/* View pour consulter un adhérent */
+CREATE VIEW ConsultationAdherent AS
+SELECT Adherents.login,Adherents.Nom ,Adherents.Prenom ,Adherents.Adresse ,Adherents.Mail,Adherents.Num_telephone,Adherents.Date_de_naissance ,Adherents.Nb_retard ,Adherents.Nb_degradation,Adherents.carte,Adhesions.Debut,Adhesions.FIN, EMPRUNT.emprunt_enCours, Reservation.état_reservation, Sanction.En_sanction
+FROM (((Adherents INNER JOIN Adhesions ON Adherents.login= Adhesions.login)INNER JOIN EMPRUNT ON EMPRUNT.login=Adhesions.login)INNER JOIN Reservation ON Reservation.login=Adhesions.login)INNER JOIN Sanction ON Sanction.login=Adhesions.login ;
+
+/* Afficher la liste des emprunts en cours */
+CREATE VIEW EmpruntEnCours AS
+SELECT Clé, login
+FROM EMPRUNT
+WHERE emprunt_enCours = true;
+
+/*Views pour avoir les personnes en cours de sanction*/
+CREATE VIEW PersonneSanction AS
+SELECT S.En_sanction
+FROM Sanction S JOIN Adherents A ON S.login = A.login ;
+
+/*Views pour avoir les personnes en cours de sanction*/
+CREATE VIEW HistoriqueEmprunt AS
+SELECT *
+FROM Emprunt E Join Adherents A ON E.login = A.login ;
+
+
+/*now, à chaque fois on rejoute le login pour rajouter une condition WHERE dans le code python et pouvoir sélectionner à partir du login ce que l'on recherche*/
+
+CREATE VIEW Genre1 AS
+SELECT Exemplaire.Code, Exemplaire.Clé, Emprunt.login
+FROM Emprunt JOIN Exemplaire ON Emprunt.Clé = Exemplaire.Clé;
+
+
+CREATE VIEW Genre2 AS
+SELECT Ressource.Genre, Genre1.Clé, Genre1.login
+FROM Genre1 Join Ressource ON Genre1.Code = Ressource.Code;
+
+/* On compte le nombre d'exemplaire emprunté avec le même genre*/
+CREATE VIEW Genre3 AS
+SELECT Genre2.login, Genre2.Genre, COUNT(Genre2.Clé) AS compte
+FROM Genre2
+GROUP BY Genre2.Genre, Genre2.login ;
+
+/*On cherche le Max de la tables ensuite dans le python*/
+CREATE VIEW Genre4 AS
+SELECT Genre3.login, Genre3.Genre, MAX(Genre3.compte)
+FROM Genre3 GROUP BY Genre3.login, Genre3.Genre;
+
+/* Exemplaire le plus emprunté de la bibliothèque  */
+CREATE VIEW Populaire AS
+SELECT Ressource.Code , Ressource.Titre , Ressource.Éditeur ,Ressource.Genre , Ressource.Type, MAX(Exemplaire.compteur)
+FROM Ressource INNER JOIN Exemplaire ON Ressource.Code=Exemplaire.Code GROUP BY Ressource.Code, Ressource.Titre, Ressource.Éditeur, Ressource.Genre , Ressource.Type;
+
+/* Exemplaire emprunté plus de 10 fois de la bibliothèque  */
+CREATE VIEW Populaire2 AS
+SELECT Ressource.Code , Ressource.Titre ,Ressource.Genre , Ressource.Type, Exemplaire.compteur
+FROM Ressource INNER JOIN Exemplaire ON Ressource.Code=Exemplaire.Code
+WHERE Exemplaire.compteur >= 10 ;
+
+/* On compte le nombre de films d'une même langue empruntés*/
+CREATE VIEW LangueFilm1 AS
+SELECT Ressource.Langue_film, Genre1.Clé, Genre1.login
+FROM Genre1 Join Ressource ON Genre1.Code = Ressource.Code WHERE Ressource.Type = 'Film';
+
+CREATE VIEW LangueFilm2 AS
+SELECT login, Langue_film, COUNT(Clé)
+FROM LangueFilm1
+GROUP BY LangueFilm1.login, LangueFilm1.Langue_film
+HAVING COUNT(Clé) >= 2;
+
+/* On compte le nombre de livres d'une même langue empruntés*/
+CREATE VIEW LangueLivre1 AS
+SELECT Ressource.Langue_livre, Genre1.Clé, Genre1.login
+FROM Genre1 Join Ressource ON Genre1.Code = Ressource.Code WHERE Ressource.Type = 'Livre';
+
+CREATE VIEW LangueLivre2 AS
+SELECT login, Langue_livre, COUNT(Clé)
+FROM LangueLivre1
+GROUP BY LangueLivre1.login, LangueLivre1.Langue_livre
+HAVING COUNT(Clé) >= 3;
+
+
+/*
+CREATE VIEW statistiques3 AS
+SELECT AVG()
+FROM Ressource R Join Exemplaire E ON R.Code = A.Code
+GROUP BY Genre;*/
+
